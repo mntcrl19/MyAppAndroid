bcf19330f76cb14183795653d802baa997cab94c
Guillermo Montecoral <guillermo.montecoral@gmail.com>
2020-10-21 13:58:40 -0300

    Subida repositorio local a GitHub

---
+++ app/src/main/java/com/mntcrl/myappandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mntcrl.myappandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
